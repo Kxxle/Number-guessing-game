3cc90f1a609024ada29bf2dbb243da6a1fe36b40
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-06-03 09:28:42 +0200

    Add project files.

---
+++ Number guessing game/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_guessing_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Choose your minimum number?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose your maximum number?");
+            int y = Convert.ToInt32(Console.ReadLine());
+            rand.Next(x,y);
+
+            Console.ReadKey();
+        }
+    }
+}
